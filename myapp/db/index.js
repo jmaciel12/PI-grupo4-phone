5075ed232ed3611afc119fbdf99e0c027ae10f0f
jmaciel <jmacielrampone@udesa.edu.ar>
2025-04-02 18:01:59 -0300

    orgaización y creación de carpetas necesarias

---
+++ myapp/db/index.js
@@ -0,0 +1,41 @@
+const celulares = {
+
+    //Lista de celulares
+    lista : [
+        {
+            id: 1,
+            nombre: "Celular 1",
+            precio: 1000,
+            imagen: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        },
+        {
+            id: 2,
+            nombre: "Celular 2",
+            precio: 2000,
+            imagen: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"                
+        },
+
+        {
+            id: 3,
+            nombre: "Celular 3",
+            precio: 3000,
+            imagen: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"                
+        },
+
+        {
+            id: 4,
+            nombre: "Celular 4",
+            precio: 4000,
+            imagen: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"                
+        },
+
+        {
+            id: 5,
+            nombre: "Celular 5",
+            precio: 5000,
+            imagen: "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"                
+        }
+    ]
+}
+
+module.exports = celulares
